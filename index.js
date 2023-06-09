768ab08c7dab29ab1a8d70699f39c2f4c81bf23a
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-06-10 03:23:27 +0600

    added api for social login

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ysflwyz.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,51 +29,71 @@ async function run() {
     await client.connect();
 
     const classCollection = client.db('artcraft').collection('classes');
+    const userCollection = client.db('artcraft').collection('users');
     const selectedClassCollection = client.db('artcraft').collection('selectedClass');
     const instructorCollection = client.db('artcraft').collection('instructor');
-    
+
+    //user related API----------------------------------------------------
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'user already exists'})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
 
     //class related api-----------------------------------------
-    app.get('/classes', async(req, res) => {
+    app.get('/classes', async (req, res) => {
       // const result = await classCollection.find().toArray();
       // res.send(result);
       const result = await classCollection.find().toArray();
-      const sortResult = result.sort((a,b)=>parseInt(b.students)-parseInt(a.students))
+      const sortResult = result.sort((a, b) => parseInt(b.students) - parseInt(a.students))
       res.send(sortResult)
     })
-    app.get('/allclasses', async(req, res) => {
+    app.get('/allclasses', async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
     })
 
     // instructor Related api----------------------------------------------------------------
-    app.get('/allinstructor', async(req, res) => {
+    app.get('/allinstructor', async (req, res) => {
       const result = await instructorCollection.find().toArray();
-      const sortResult = result.sort((a,b)=>parseInt(b.students)-parseInt(a.students))
+      const sortResult = result.sort((a, b) => parseInt(b.students) - parseInt(a.students))
       res.send(sortResult)
     })
-    app.get('/instructor', async(req, res) => {
+    app.get('/instructor', async (req, res) => {
       const result = await instructorCollection.find().toArray();
       res.send(result);
     })
 
 
     //selected Related API---------------------------------------------
-    app.post('/selectedclass', async(req, res) => {
+    app.post('/selectedclass', async (req, res) => {
       const item = req.body;
       const result = await selectedClassCollection.insertOne(item)
       res.send(result);
     })
 
-    app.get('/selectedclass', async(req, res)=> {
+    app.get('/selectedclass', async (req, res) => {
       const email = req.query.email;
       // console.log(email)
-      if(!email){
+      if (!email) {
         return res.send([])
       }
-      const query = {email: email};
+      const query = { email: email };
       const result = await selectedClassCollection.find(query).toArray();
       res.send(result);
+    })
+
+    app.delete('/selectedclass/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassCollection.deleteOne(query);
+      res.send(result)
     })
 
 
@@ -91,8 +111,8 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('assignment twelve is running')
+  res.send('assignment twelve is running')
 })
 app.listen(port, () => {
-    console.log(`assignment twelve runnint on port ${port}`)
+  console.log(`assignment twelve runnint on port ${port}`)
 })
