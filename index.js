944b68b3f304599d779e1368658ba36ba2311900
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-06-10 23:28:01 +0600

    added admin api

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  //bearer and token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -68,6 +86,13 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
+    app.get('/users/admin/:email', async(req, res) => {
+      const email = req.params.email;
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      const result = {admin: user?.role === 'admin'}
+      res.send(result)
+    })
 
     //---------------------------api for creating an instructor--------------------------
     app.patch('/users/instructor/:id', async(req, res) => {
@@ -121,6 +146,12 @@ async function run() {
       if (!email) {
         return res.send([])
       }
+
+      // const decodedEmail = req.decoded.email;
+      // if(email !== decodedEmail){
+      //   return res.status(403).send({error: true, message:'forbidden access'})
+      // }
+
       const query = { email: email };
       const result = await selectedClassCollection.find(query).toArray();
       res.send(result);
